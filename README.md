424d88436e4cf786d6655fc51b7dd9efefba7f04
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-07-25 13:16:55 -0600

    demo script + overview, demo for README

---
+++ README.md
@@ -0,0 +1,200 @@
+# Proof-of-Concept: Privacy-Conscious Age Verification
+
+*(To take a quick look, you can look at the overview and the high-level workflow for the demo&mdash;and
+maybe the entire demo.)*
+
+## Overview
+
+As age verification mandates for social media have gained traction on both the state and federal level,
+some tech policy experts have said that age verification and privacy cannot coexist.
+[This quote][rstreet-dne] is a representative example of what those experts have said:
+"The technology to verify your age without violating your privacy does not exist."
+
+But are those experts right? Simply put, no. But rather than tell you why, I will show you&mdash;by
+writing proof-of-concept code for privacy-conscious age verification:
+
+- It provides robust age verification, with multiple measures to minimize the number of kids who bypass the system.
+- For anonymous accounts, it verifies their age without de-anonymizing them, via multiple layers of security.
+- It minimizes the impact of data breaches; users won't get de-anonymized if a data breach occurs.
+
+Any tradeoffs were weighed heavily in favor of privacy. The system is not designed to be 100% effective,
+but it will still be very effective&mdash;while protecting the anonymity of users.
+
+## Demo
+
+### High-Level Workflow
+
+Since it's easier to show than tell, let's run a demo.
+This demo will run the code, and it will display some text to explain what is happening.
+
+Here is the first part of the demo, where a parent and child verify their accounts on two social media sites:
+
+```
+In this proof-of-concept:
+- CheckMyAge is a third-party age verification service.
+- Crackle and Pop are social media sites that have registered with CheckMyAge.
+- Crackle and Pop need to know whether a user's age is 12-, 13-17, 18+.
+
+John Smith has already verified his identity on CheckMyAge:
+- Age: 40
+Bobby Smith has already verified his identity on CheckMyAge:
+- Age: 13
+- Guardian: John Smith
+
+John Smith uses CheckMyAge to verify "JohnS" on Crackle:
+- Age: 18+
+Bobby Smith uses CheckMyAge to verify "BobbyS" on Crackle:
+- Age: 13-17
+- Guardian: JohnS
+
+John Smith uses CheckMyAge to verify "publius" on Pop:
+- Age: 18+
+Bobby Smith uses CheckMyAge to verify "publius-jr" on Pop:
+- Age: 13-17
+- Guardian: publius
+```
+
+In the second part of the demo, we'll look slightly under the hood to see what data is being stored:
+
+```
+Data stored on CheckMyAge:
+- John Smith:
+    - ID: uhzmISXl7szUDLVuYNvDVf6jiL3ExwCybtg-KlazHU4
+    - Age: 40
+- Bobby Smith:
+    - ID: KB0b9pDo8j7-1p90fFokbgHj8hzbbU7jCGGjfuMzLR4
+    - Age: 13
+    - Guardian ID: uhzmISXl7szUDLVuYNvDVf6jiL3ExwCybtg-KlazHU4
+
+Data stored on Crackle:
+- JohnS:
+    - ID: wqhgWlb9wYtzTDYbGeYFJJvS4xjmQsp3cf3ntbcBuNI
+    - Age: 18+
+- BobbyS:
+    - ID: vT47RJUVsiagXQvHACvJKjliGLM97QcBrFRk9PfmAxE
+    - Age: 13-17
+    - Guardian ID: wqhgWlb9wYtzTDYbGeYFJJvS4xjmQsp3cf3ntbcBuNI
+
+Data stored on Pop:
+- publius:
+    - ID: Uum6yHO7tgND6ffCHsidSpghQz8Eq7PlkmWHzkVL2DE
+    - Age: 18+
+- publius-jr:
+    - ID: 5L3siUtZ5-HAKlxMGjwvEjFB49FoLJdSpytsuE6WeQU
+    - Age: 13-17
+    - Guardian ID: Uum6yHO7tgND6ffCHsidSpghQz8Eq7PlkmWHzkVL2DE
+```
+
+The IDs serve three purposes:
+
+1. They provide anonymity; CheckMyAge only shares IDs and age ranges (e.g., 13-17, 18+) with Crackle and Pop.
+2. They verify that one user is the guardian of another user&mdash;without revealing the identity of those users.
+3. They ensure that one person cannot verify multiple accounts on a single site.
+
+Each person also has a different ID for CheckMyAge, Crackle, and Pop:
+
+- If both CheckMyAge and Pop suffered data breaches, you could not figure out that `publius` is John Smith.
+- If both Crackle and Pop suffered data breaches,
+  you could not figure out that `JohnS` and `publius` are the same person.
+
+(If one engineer could accomplish this, imagine what a team of engineers with even more expertise could accomplish.)
+
+### Detailed Workflow
+
+The third part of the demo looks at the workflow to verify an account in more detail:
+
+```
+Detailed workflow to verify "publius-jr" on Pop:
+- The current time is July 25, 2023, 12:42:56 PM MDT.
+- [#1], [#2] is used to denote when data is stored that will be used later.
+
+For a proof-of-concept, everything runs on a single machine;
+the demo pretends that CheckMyAge and Pop are separate websites.
+(For the real thing, we assume that engineers know how to build a website.)
+
+Part I
+- Bobby Smith logs in as "publius-jr" on Pop.
+- Bobby Smith starts the process to verify "publius-jr".
+- Pop asks CheckMyAge to create a new verification request.
+- CheckMyAge generates the following verification request:
+    - Request ID: 7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+    - Site: Pop
+    - Expiration: July 25, 2023, 12:47:56 PM MDT
+- [#1] CheckMyAge stores the verification request.
+- CheckMyAge sends the verification request back to Pop.
+- [#2] Pop links the request ID (7-DQzGbL...) to "publius-jr".
+- Pop opens the following URL in a new window on Bobby Smith's browser:
+  https://www.checkmyage.com/verify/7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+
+Part II
+- CheckMyAge receives a web request at the following URL:
+  https://www.checkmyage.com/verify/7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+- CheckMyAge gets the verification request ID (7-DQzGbL...) from the URL.
+- [#1] CheckMyAge retrieves the full verification request:
+    - Request ID: 7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+    - Site: Pop
+    - Expiration: July 25, 2023, 12:47:56 PM MDT
+- CheckMyAge checks that the verification request is not expired.
+- CheckMyAge responds to the web request by displaying a login page.
+- Bobby Smith logs in to CheckMyAge.
+- CheckMyAge links the request ID (7-DQzGbL...) to Bobby Smith.
+- CheckMyAge asks Bobby Smith if he wants to verify an account on Pop.
+- Bobby Smith confirms that he wants to verify an account on Pop.
+- CheckMyAge retrieves the user data for Bobby Smith:
+    - ID: KB0b9pDo8j7-1p90fFokbgHj8hzbbU7jCGGjfuMzLR4
+    - Age: 13
+    - Guardian ID: uhzmISXl7szUDLVuYNvDVf6jiL3ExwCybtg-KlazHU4
+- CheckMyAge anonymizes the age that it will share with Pop: 13-17
+- CheckMyAge creates new IDs from the original IDs and its secret key for Pop.
+- Updated user data:
+    - ID: A8y9RGWwLiwhZSaX0i_TZhyX-2r9DxMmrrngoADCUhE
+    - Age: 13-17
+    - Guardian ID: iaDG-BXou0kKr5gg2j0BJj0RKsa00bVvnpbRCiEism4
+- CheckMyAge creates the age certificate:
+    - Request ID: 7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+    - Site: Pop
+    - Expiration: July 25, 2023, 12:47:56 PM MDT
+    - ID: A8y9RGWwLiwhZSaX0i_TZhyX-2r9DxMmrrngoADCUhE
+    - Age: 13-17
+    - Guardian ID: iaDG-BXou0kKr5gg2j0BJj0RKsa00bVvnpbRCiEism4
+- CheckMyAge digitally signs the age certificate.
+- CheckMyAge securely transmits the signed age certificate to Pop.
+
+Part III
+- Pop receives a signed age certificate:
+    - Request ID: 7-DQzGbLlaMeQkwMWE_AGDXgFnbWfRR_P0UkwYEGbZk
+    - Site: Pop
+    - Expiration: July 25, 2023, 12:47:56 PM MDT
+    - ID: A8y9RGWwLiwhZSaX0i_TZhyX-2r9DxMmrrngoADCUhE
+    - Age: 13-17
+    - Guardian ID: iaDG-BXou0kKr5gg2j0BJj0RKsa00bVvnpbRCiEism4
+- Pop verifies the signed age certificate.
+    - It verifies that the age certificate is signed by CheckMyAge.
+    - It verifies that Pop is the recipient in the "Site" field.
+    - It verifies that the age certificate is not expired.
+- [#2] Pop matches the request ID (7-DQzGbL...) to "publius-jr".
+- Pop extracts the user data from the age certificate:
+    - ID: A8y9RGWwLiwhZSaX0i_TZhyX-2r9DxMmrrngoADCUhE
+    - Age: 13-17
+    - Guardian ID: iaDG-BXou0kKr5gg2j0BJj0RKsa00bVvnpbRCiEism4
+- Pop creates new IDs from the original IDs and a secret key.
+- Updated user data:
+    - ID: 5L3siUtZ5-HAKlxMGjwvEjFB49FoLJdSpytsuE6WeQU
+    - Age: 13-17
+    - Guardian ID: Uum6yHO7tgND6ffCHsidSpghQz8Eq7PlkmWHzkVL2DE
+- Pop checks that no other accounts have the same user ID (5L3siUtZ...).
+- Pop stores this user data for "publius-jr". "publius-jr" is now verified!
+```
+
+*(If you run the demo yourself, the times and request ID will be different.)*
+
+Notes:
+
+- Communication between CheckMyAge and Pop is mediated via a request ID, which is randomly generated.
+    - Pop never shares the username (`publius-jr`) with CheckMyAge.
+    - CheckMyAge never shares the real name (Bobby Smith) with Pop.
+- The age certificate contains an "intermediate ID."
+    - CheckMyAge never sees the ID stored on Pop (`5L3siUtZ...`).
+    - Pop never sees the ID stored on CheckMyAge (`KB0b9pDo...`).
+
+[rstreet-dne]: https://www.rstreet.org/commentary/the-technology-to-verify-your-age-without-violating-your-privacy-does-not-exist/
--- demo/src/main/java/org/example/age/demo/Main.java
+++ demo/src/main/java/org/example/age/demo/Main.java
@@ -1,11 +1,275 @@
 package org.example.age.demo;
 
+import com.google.errorprone.annotations.FormatMethod;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.example.age.certificate.AgeCertificate;
+import org.example.age.certificate.VerificationRequest;
+import org.example.age.data.AgeRange;
+import org.example.age.data.AgeThresholds;
+import org.example.age.data.SecureId;
+import org.example.age.data.VerifiedUser;
+import org.example.age.verification.AvsUi;
+import org.example.age.verification.SiteUi;
+import org.example.age.verification.VerifiedUserStore;
+
 /** Demos a proof-of-concept for age verification. */
 public final class Main {
 
+    private static final Demo demo = Demo.create();
+
     /** Main method. */
     public static void main(String[] args) {
-        System.out.println("TODO");
+        displayBorder();
+
+        // high-level workflow for all sites
+        verifyParentAndChildForAvs();
+        verifyParentAndChildForSite(Demo.SITE1_NAME, Demo.PARENT_SITE1_USERNAME, Demo.CHILD_SITE1_USERNAME);
+        verifyParentAndChildForSite(Demo.SITE2_NAME, Demo.PARENT_SITE2_USERNAME, Demo.CHILD_SITE2_USERNAME);
+        displayBorder();
+
+        // display the user data stored
+        displayParentAndChildDataForSite(Demo.AVS_NAME, Demo.PARENT_REAL_NAME, Demo.CHILD_REAL_NAME);
+        displayParentAndChildDataForSite(Demo.SITE1_NAME, Demo.PARENT_SITE1_USERNAME, Demo.CHILD_SITE1_USERNAME);
+        displayParentAndChildDataForSite(Demo.SITE2_NAME, Demo.PARENT_SITE2_USERNAME, Demo.CHILD_SITE2_USERNAME);
+        displayBorder();
+
+        // detailed workflow for a single account
+        verifyUserForSiteVerbose(Demo.SITE2_NAME, Demo.CHILD_REAL_NAME, Demo.CHILD_SITE2_USERNAME);
+        displayBorder();
+    }
+
+    /** Verifies the age and guardian relationship of a parent and child for the age verification service. */
+    private static void verifyParentAndChildForAvs() {
+        displayIntro();
+        verifyPersonForAvs(Demo.PARENT_REAL_NAME, Demo.PARENT_AGE, Optional.empty());
+        verifyPersonForAvs(Demo.CHILD_REAL_NAME, Demo.CHILD_AGE, Optional.of(Demo.PARENT_REAL_NAME));
+        println();
+    }
+
+    /** Displays some introductory text. */
+    private static void displayIntro() {
+        String avsName = demo.avsUi().getName();
+        String site1Name = demo.siteUi(Demo.SITE1_NAME).getName();
+        String site2Name = demo.siteUi(Demo.SITE2_NAME).getName();
+
+        println("In this proof-of-concept:");
+        println("- %s is a third-party age verification service.", avsName);
+        println("- %s and %s are social media sites that have registered with %s.", site1Name, site2Name, avsName);
+        println(
+                "- %s and %s need to know whether a user's age is %s.",
+                site1Name, site2Name, ageThresholds(Demo.AGE_THRESHOLDS));
+        println();
+    }
+
+    /** Verifies the age and guardian (if applicable) of a person for the age verification service. */
+    private static void verifyPersonForAvs(String realName, int age, Optional<String> maybeGuardian) {
+        println("%s has already verified his identity on %s:", realName, Demo.AVS_NAME);
+        println("- Age: %d", age);
+        if (maybeGuardian.isPresent()) {
+            String guardian = maybeGuardian.get();
+            println("- Guardian: %s", guardian);
+        }
+    }
+
+    /** Verifies the age and guardian relationship of a parent and child for a site. */
+    private static void verifyParentAndChildForSite(String siteId, String parentUsername, String childUsername) {
+        AvsUi avsUi = demo.avsUi();
+        SiteUi siteUi = demo.siteUi(siteId);
+
+        verifyUserForSite(avsUi, siteUi, Demo.PARENT_REAL_NAME, parentUsername);
+        verifyUserForSite(avsUi, siteUi, Demo.CHILD_REAL_NAME, childUsername);
+        println();
+    }
+
+    /** Verifies the age and guardian (if applicable) of a user for a site. */
+    private static void verifyUserForSite(AvsUi avsUi, SiteUi siteUi, String realName, String username) {
+        // Run the workflow.
+        VerificationRequest request = siteUi.createVerificationRequest(username);
+        avsUi.processVerificationRequest(realName, request.id());
+
+        // Display the results.
+        println("%s uses %s to verify \"%s\" on %s:", realName, avsUi.getName(), username, siteUi.getName());
+        println("- Age: %s", siteUi.getAgeRange(username));
+        if (!siteUi.getGuardians(username).isEmpty()) {
+            String guardian = siteUi.getGuardians(username).get(0);
+            println("- Guardian: %s", guardian);
+        }
+    }
+
+    /** Displays the data for a parent and child that is stored on a site or service. */
+    private static void displayParentAndChildDataForSite(String name, String parentUsername, String childUsername) {
+        VerifiedUserStore userStore = demo.verifiedUserStore(name);
+
+        println("Data stored on %s:", name);
+        displayUserDataForSite(userStore, parentUsername);
+        displayUserDataForSite(userStore, childUsername);
+        println();
+    }
+
+    /** Displays the data for a user that is stored on the site or service. */
+    private static void displayUserDataForSite(VerifiedUserStore userStore, String username) {
+        VerifiedUser user = userStore.retrieveVerifiedUser(username);
+
+        println("- %s:", username);
+        displayVerifiedUser(user);
+    }
+
+    /** Verifies the age and guardian (if applicable) of a user for a site. Displays verbose output. */
+    private static void verifyUserForSiteVerbose(String siteId, String realName, String username) {
+        AvsUi avsUi = demo.avsUi();
+        SiteUi siteUi = demo.siteUi(siteId);
+        VerifiedUserStore avsUserStore = demo.verifiedUserStore(Demo.AVS_NAME);
+        VerifiedUserStore siteUserStore = demo.verifiedUserStore(siteId);
+
+        // Run the workflow.
+        VerificationRequest request = siteUi.createVerificationRequest(username);
+        avsUi.processVerificationRequest(realName, request.id());
+
+        // Get verbose information.
+        VerifiedUser avsUser = avsUserStore.retrieveVerifiedUser(realName);
+        AgeCertificate certificate = demo.ageCertificate(request.id());
+        VerifiedUser siteUser = siteUserStore.retrieveVerifiedUser(username);
+
+        // Display the detailed workflow.
+        String avsName = avsUi.getName();
+        String siteName = siteUi.getName();
+
+        println("Detailed workflow to verify \"%s\" on %s:", username, siteName);
+        println("- The current time is %s.", time(ZonedDateTime.now(ZoneId.systemDefault())));
+        println("- [#1], [#2] is used to denote when data is stored that will be used later.");
+        println();
+        println("For a proof-of-concept, everything runs on a single machine;");
+        println("the demo pretends that %s and %s are separate websites.", avsName, siteName);
+        println("(For the real thing, we assume that engineers know how to build a website.)");
+        println();
+        println("Part I");
+        println("- %s logs in as \"%s\" on %s.", realName, username, siteName);
+        println("- %s starts the process to verify \"%s\".", realName, username);
+        println("- %s asks %s to create a new verification request.", siteName, avsName);
+        println("- %s generates the following verification request:", avsName);
+        displayVerificationRequest(request);
+        println("- [#1] %s stores the verification request.", avsName);
+        println("- %s sends the verification request back to %s.", avsName, siteName);
+        println("- [#2] %s links the request ID (%s) to \"%s\".", siteName, shortId(request.id()), username);
+        println("- %s opens the following URL in a new window on %s's browser:", siteName, realName);
+        println("  %s", avsUrl(avsName, request.id()));
+        println();
+
+        println("Part II");
+        println("- %s receives a web request at the following URL:", avsName);
+        println("  %s", avsUrl(avsName, request.id()));
+        println("- %s gets the verification request ID (%s) from the URL.", avsName, shortId(request.id()));
+        println("- [#1] %s retrieves the full verification request:", avsName);
+        displayVerificationRequest(request);
+        println("- %s checks that the verification request is not expired.", avsName);
+        println("- %s responds to the web request by displaying a login page.", avsName);
+        println("- %s logs in to %s.", realName, avsName);
+        println("- %s links the request ID (%s) to %s.", avsName, shortId(request.id()), realName);
+        println("- %s asks %s if he wants to verify an account on %s.", avsName, realName, siteName);
+        println("- %s confirms that he wants to verify an account on %s.", realName, siteName);
+        println("- %s retrieves the user data for %s:", avsName, realName);
+        displayVerifiedUser(avsUser);
+        println(
+                "- %s anonymizes the age that it will share with %s: %s",
+                avsName, siteName, certificate.verifiedUser().ageRange());
+        println("- %s creates new IDs from the original IDs and its secret key for %s.", avsName, siteName);
+        println("- Updated user data:");
+        displayVerifiedUser(certificate.verifiedUser());
+        println("- %s creates the age certificate:", avsName);
+        displayAgeCertificate(certificate);
+        println("- %s digitally signs the age certificate.", avsName);
+        println("- %s securely transmits the signed age certificate to %s.", avsName, siteName);
+        println();
+
+        println("Part III");
+        println("- %s receives a signed age certificate:", siteName);
+        displayAgeCertificate(certificate);
+        println("- %s verifies the signed age certificate.", siteName);
+        println("    - It verifies that the age certificate is signed by %s.", avsName);
+        println("    - It verifies that %s is the recipient in the \"Site\" field.", siteName);
+        println("    - It verifies that the age certificate is not expired.");
+        println("- [#2] %s matches the request ID (%s) to \"%s\".", siteName, shortId(request.id()), username);
+        println("- %s extracts the user data from the age certificate:", siteName);
+        displayVerifiedUser(certificate.verifiedUser());
+        println("- %s creates new IDs from the original IDs and a secret key.", siteName);
+        println("- Updated user data:");
+        displayVerifiedUser(siteUser);
+        println("- %s checks that no other accounts have the same user ID (%s).", siteName, shortId(siteUser.id()));
+        println("- %s stores this user data for \"%s\". \"%s\" is now verified!", siteName, username, username);
+        println();
+    }
+
+    /** Displays an age certificate. */
+    private static void displayAgeCertificate(AgeCertificate certificate) {
+        displayVerificationRequest(certificate.verificationRequest());
+        displayVerifiedUser(certificate.verifiedUser());
+    }
+
+    /** Displays a verification request. */
+    private static void displayVerificationRequest(VerificationRequest request) {
+        println("    - Request ID: %s", request.id());
+        println("    - Site: %s", request.siteId());
+        println("    - Expiration: %s", time(request.expiration()));
+    }
+
+    /** Displays a verified user. */
+    private static void displayVerifiedUser(VerifiedUser user) {
+        println("    - ID: %s", user.id());
+        println("    - Age: %s", user.ageRange());
+        if (!user.guardianIds().isEmpty()) {
+            SecureId guardianId = user.guardianIds().get(0);
+            println("    - Guardian ID: %s", guardianId);
+        }
+    }
+
+    /** Formats age thresholds. */
+    private static String ageThresholds(AgeThresholds ageThresholds) {
+        List<AgeRange> ageRanges = ageThresholds.getAgeRanges();
+        int lastIndex = ageRanges.size() - 1;
+        String last = ageRanges.get(lastIndex).toString();
+        String others =
+                ageRanges.subList(0, lastIndex).stream().map(AgeRange::toString).collect(Collectors.joining(", "));
+        return String.format("%s, %s", others, last);
+    }
+
+    /** Formats the URL to verify an account on the age verification service. */
+    private static String avsUrl(String avsName, SecureId requestId) {
+        return String.format("https://www.%s.com/verify/%s", avsName.toLowerCase(Locale.US), requestId);
+    }
+
+    /** Formats an ID in a shortened form. */
+    private static String shortId(SecureId id) {
+        String idText = id.toString();
+        return String.format("%s...", idText.substring(0, 8));
+    }
+
+    /** Formats a time. */
+    private static String time(ZonedDateTime dateTime) {
+        ZonedDateTime localExpiration = dateTime.withZoneSameInstant(ZoneId.systemDefault());
+        return localExpiration.format(DateTimeFormatter.ofPattern("LLLL d, yyyy, h:mm:ss a z"));
+    }
+
+    /** Displays a border. */
+    private static void displayBorder() {
+        println("================================================================================");
+        println();
+    }
+
+    /** Syntactic sugar for printing a line that may be formatted. */
+    @FormatMethod
+    private static void println(String format, Object... args) {
+        System.out.format(format, args).println();
+    }
+
+    /** Syntactic sugar for printing an empty line. */
+    private static void println() {
+        System.out.println();
     }
 
     // static class
