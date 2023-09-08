21addbd07d245ea18bad3586a804f91a486abdfc
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-09-07 20:39:24 -0600

    add common state object for verification (#33)

---
+++ common/src/main/java/org/example/age/common/verification/VerificationStatus.java
@@ -0,0 +1,9 @@
+package org.example.age.common.verification;
+
+/** Age verification status for a user. */
+public enum VerificationStatus {
+    UNVERIFIED,
+    EXPIRED,
+    INVALIDATED,
+    VERIFIED,
+}
