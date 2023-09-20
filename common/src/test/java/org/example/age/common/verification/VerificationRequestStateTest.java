4aba80225cc534d82f21b8da8c2a93f1bffc64ac
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-09-19 21:23:30 -0600

    add verification request state

---
+++ common/src/test/java/org/example/age/common/verification/VerificationRequestStateTest.java
@@ -0,0 +1,57 @@
+package org.example.age.common.verification;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.time.Duration;
+import org.assertj.core.api.ThrowableAssert;
+import org.example.age.certificate.AuthToken;
+import org.example.age.certificate.VerificationRequest;
+import org.junit.jupiter.api.Test;
+
+public final class VerificationRequestStateTest {
+
+    private static final VerificationRequest REQUEST =
+            VerificationRequest.generateForSite("Site", Duration.ofMinutes(5));
+    private static final AuthToken TOKEN = AuthToken.empty();
+
+    @Test
+    public void inactive() {
+        VerificationRequestState state = VerificationRequestState.inactive();
+        assertThat(state.status()).isEqualTo(VerificationRequestStatus.INACTIVE);
+    }
+
+    @Test
+    public void pending() {
+        VerificationRequestState state = VerificationRequestState.pending(REQUEST, TOKEN);
+        assertThat(state.status()).isEqualTo(VerificationRequestStatus.PENDING);
+        assertThat(state.verificationRequest()).isEqualTo(REQUEST);
+        assertThat(state.localAuthToken()).isEqualTo(TOKEN);
+    }
+
+    @Test
+    public void expired() {
+        VerificationRequestState state = VerificationRequestState.expired(REQUEST);
+        assertThat(state.status()).isEqualTo(VerificationRequestStatus.EXPIRED);
+        assertThat(state.verificationRequest()).isEqualTo(REQUEST);
+    }
+
+    @Test
+    public void error_AttributeNotSet_Inactive() {
+        String message = "attribute not set when the status is INACTIVE";
+        VerificationRequestState state = VerificationRequestState.inactive();
+        error_AttributeNotSet(() -> state.verificationRequest(), message);
+        error_AttributeNotSet(() -> state.localAuthToken(), message);
+    }
+
+    @Test
+    public void error_AttributeNotSet_Expired() {
+        String message = "attribute not set when the status is EXPIRED";
+        VerificationRequestState state = VerificationRequestState.expired(REQUEST);
+        error_AttributeNotSet(() -> state.localAuthToken(), message);
+    }
+
+    private void error_AttributeNotSet(ThrowableAssert.ThrowingCallable callable, String message) {
+        assertThatThrownBy(callable).isInstanceOf(IllegalStateException.class).hasMessage(message);
+    }
+}
