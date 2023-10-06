862a88ba11e2094f72a201ae8056338a361a91cd
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-10-06 15:32:46 -0600

    add site configuration (#52)

---
+++ common/src/main/java/org/example/age/common/config/SiteConfigurerModule.java
@@ -0,0 +1,53 @@
+package org.example.age.common.config;
+
+import com.google.common.net.HostAndPort;
+import dagger.Module;
+import dagger.Provides;
+import java.security.PublicKey;
+import java.time.Duration;
+import java.util.function.Supplier;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+/**
+ * Dagger modules that binds dependencies for...
+ * <ul>
+ *     <li><code>@Named("avs") Supplier&lt;{@link HostAndPort}&gt;</code></li>
+ *     <li><code>@Named("avs") Supplier&lt;{@link PublicKey}&gt;</code></li>
+ *     <li><code>@Named("siteId") Supplier&lt;String&gt;</code></li>
+ *     <li><code>@Named("expiresIn") Supplier&lt;{@link Duration}&gt;</code></li>
+ * </ul>
+ *
+ * <p>Depends on an unbound <code>Supplier&lt;{@link SiteConfig}&gt;</code>.</p>
+ */
+@Module
+public interface SiteConfigurerModule {
+
+    @Provides
+    @Named("avs")
+    @Singleton
+    static Supplier<HostAndPort> provideAvsHostAndPort(Supplier<SiteConfig> siteConfigSupplier) {
+        return () -> siteConfigSupplier.get().avsHostAndPort();
+    }
+
+    @Provides
+    @Named("avs")
+    @Singleton
+    static Supplier<PublicKey> provideAvsPublicKey(Supplier<SiteConfig> siteConfigSupplier) {
+        return () -> siteConfigSupplier.get().avsPublicKey();
+    }
+
+    @Provides
+    @Named("siteId")
+    @Singleton
+    static Supplier<String> provideSiteId(Supplier<SiteConfig> siteConfigSupplier) {
+        return () -> siteConfigSupplier.get().siteId();
+    }
+
+    @Provides
+    @Named("expiresIn")
+    @Singleton
+    static Supplier<Duration> provideExpiresIn(Supplier<SiteConfig> siteConfigSupplier) {
+        return () -> siteConfigSupplier.get().expiresIn();
+    }
+}
