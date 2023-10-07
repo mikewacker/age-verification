4af90aa7b7279c942f4c151f209d8cce99e2d57f
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-10-06 23:16:17 -0600

    minor: module conventions for auth package

--- common/src/main/java/org/example/age/common/verification/auth/UserAgentAuthMatchDataExtractorModule.java
+++ common/src/main/java/org/example/age/common/verification/auth/UserAgentAuthMatchDataExtractorModule.java
@@ -3,9 +3,12 @@ package org.example.age.common.verification.auth;
 import dagger.Binds;
 import dagger.Module;
 
-/** Publishes a binding for {@link AuthMatchDataExtractor} that matches the {@code User-Agent} header. */
+/**
+ * Dagger module that publishes a binding for {@link AuthMatchDataExtractor},
+ * which matches the {@code User-Agent} header.
+ */
 @Module
-public interface UserAgentAuthMatchModule {
+public interface UserAgentAuthMatchDataExtractorModule {
 
     @Binds
     AuthMatchDataExtractor bindAuthMatchDataExtractor(UserAgentAuthMatchDataExtractor impl);
