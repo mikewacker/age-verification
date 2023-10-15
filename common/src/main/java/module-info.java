4ebb5e42fa5b6ed400cfd4476fae1645a77de8cf
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-10-14 21:51:25 -0600

    add module-info.java to common, adult (#64)

---
+++ common/src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module org.example.age.common {
+    exports org.example.age.common.account;
+    exports org.example.age.common.html;
+    exports org.example.age.common.site.api;
+    exports org.example.age.common.site.auth;
+    exports org.example.age.common.site.config;
+    exports org.example.age.common.site.verification;
+
+    requires static org.immutables.value.annotations;
+    requires com.google.common;
+    requires com.google.errorprone.annotations;
+    requires dagger;
+    requires java.compiler;
+    requires javax.inject;
+    requires jsr305;
+    requires okhttp3;
+    requires org.example.age.data;
+    requires undertow.core;
+    requires xnio.api;
+}
