01d56b08cc5b1786b28a54faf34b1dea64badd38
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-11-21 17:20:38 -0700

    use JsonSerializer in InMemoryPendingStore/Factory (#147)

--- common-service/src/main/java/org/example/age/common/service/store/InMemoryPendingStoreFactory.java
+++ common-service/src/main/java/org/example/age/common/service/store/InMemoryPendingStoreFactory.java
@@ -1,31 +1,31 @@
 package org.example.age.common.service.store;
 
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import org.example.age.api.JsonSerializer;
 
 /** Factory that creates {@link InMemoryPendingStore}'s. */
 @Singleton
 final class InMemoryPendingStoreFactory implements PendingStoreFactory {
 
-    private final ObjectMapper mapper;
+    private final JsonSerializer serializer;
 
     private final Map<String, InMemoryPendingStore<?>> stores = new ConcurrentHashMap<>();
 
     @Inject
-    public InMemoryPendingStoreFactory(ObjectMapper mapper) {
-        this.mapper = mapper;
+    public InMemoryPendingStoreFactory(JsonSerializer serializer) {
+        this.serializer = serializer;
     }
 
     @Override
     @SuppressWarnings("unchecked")
     public <V> PendingStore<V> getOrCreate(String name, TypeReference<V> valueTypeRef) {
         InMemoryPendingStore<?> store =
-                stores.computeIfAbsent(name, n -> new InMemoryPendingStore<>(mapper, valueTypeRef));
+                stores.computeIfAbsent(name, n -> new InMemoryPendingStore<>(serializer, valueTypeRef));
         checkValueTypeConsistent(store, valueTypeRef.getType());
         return (PendingStore<V>) store;
     }
--- common-service/src/main/java/org/example/age/common/service/store/InMemoryPendingStoreFactoryModule.java
+++ common-service/src/main/java/org/example/age/common/service/store/InMemoryPendingStoreFactoryModule.java
@@ -3,13 +3,14 @@ package org.example.age.common.service.store;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dagger.Binds;
 import dagger.Module;
+import org.example.age.infra.api.data.JsonSerializerModule;
 
 /**
  * Dagger module that publishes a binding for {@link PendingStoreFactory}, which creates in-memory stores.
  *
  * <p>Depends on an unbound {@link ObjectMapper}.</p>
  */
-@Module
+@Module(includes = JsonSerializerModule.class)
 public interface InMemoryPendingStoreFactoryModule {
 
     @Binds
