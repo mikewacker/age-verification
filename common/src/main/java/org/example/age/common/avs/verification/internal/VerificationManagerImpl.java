1844a09dd689677ad407b1aea6da7c64056ba56f
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-11-26 14:13:16 -0700

    remove/replace legacy base package from common module

--- common/src/main/java/org/example/age/common/avs/verification/internal/VerificationManagerImpl.java
+++ common/src/main/java/org/example/age/common/avs/verification/internal/VerificationManagerImpl.java
@@ -1,5 +1,6 @@
 package org.example.age.common.avs.verification.internal;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.StatusCodes;
 import java.time.Duration;
@@ -14,27 +15,29 @@ import org.example.age.common.api.data.AuthMatchDataExtractor;
 import org.example.age.common.avs.config.RegisteredSiteConfig;
 import org.example.age.common.avs.store.RegisteredSiteConfigStore;
 import org.example.age.common.avs.store.VerifiedUserStore;
-import org.example.age.common.base.store.PendingStore;
-import org.example.age.common.base.store.PendingStoreFactory;
-import org.example.age.common.base.utils.internal.PendingStoreUtils;
 import org.example.age.common.service.config.SiteLocation;
 import org.example.age.common.service.crypto.internal.AuthMatchDataEncryptor;
+import org.example.age.common.service.store.PendingStore;
+import org.example.age.common.service.store.PendingStoreFactory;
 import org.example.age.data.certificate.AgeCertificate;
 import org.example.age.data.certificate.VerificationRequest;
 import org.example.age.data.certificate.VerificationSession;
 import org.example.age.data.crypto.AesGcmEncryptionPackage;
 import org.example.age.data.crypto.SecureId;
 import org.example.age.data.user.VerifiedUser;
+import org.xnio.XnioExecutor;
 
 @Singleton
 final class VerificationManagerImpl implements VerificationManager {
+
+    private static final String UNLINKED_STORE_NAME = "unlinked";
+    private static final String LINKED_STORE_NAME = "linked";
 
     private final AuthMatchDataExtractor authDataExtractor;
     private final RegisteredSiteConfigStore siteConfigStore;
     private final VerifiedUserStore userStore;
     private final AuthMatchDataEncryptor authDataEncryptor;
-    private final PendingStore<SecureId, PendingVerification> unlinkedPendingVerifications;
-    private final PendingStore<String, PendingVerification> linkedPendingVerifications;
+    private final PendingStoreFactory pendingStoreFactory;
     private final Provider<Duration> expiresInProvider;
 
     @Inject
@@ -49,8 +52,7 @@ final class VerificationManagerImpl implements VerificationManager {
         this.siteConfigStore = siteConfigStore;
         this.userStore = userStore;
         this.authDataEncryptor = authDataEncryptor;
-        unlinkedPendingVerifications = pendingStoreFactory.create();
-        linkedPendingVerifications = pendingStoreFactory.create();
+        this.pendingStoreFactory = pendingStoreFactory;
         this.expiresInProvider = expiresInProvider;
     }
 
@@ -65,8 +67,7 @@ final class VerificationManagerImpl implements VerificationManager {
         VerificationSession session = createVerificationSession(siteId);
         PendingVerification pendingVerification = new PendingVerification(session, siteConfig);
         SecureId requestId = session.verificationRequest().id();
-        PendingStoreUtils.putForVerificationSession(
-                unlinkedPendingVerifications, requestId, pendingVerification, session, exchange);
+        putPendingVerification(UNLINKED_STORE_NAME, requestId.toString(), pendingVerification, exchange);
         return HttpOptional.of(session);
     }
 
@@ -77,15 +78,14 @@ final class VerificationManagerImpl implements VerificationManager {
             return StatusCodes.FORBIDDEN;
         }
 
-        Optional<PendingVerification> maybePendingVerification = unlinkedPendingVerifications.tryRemove(requestId);
+        Optional<PendingVerification> maybePendingVerification =
+                tryRemovePendingVerification(UNLINKED_STORE_NAME, requestId.toString());
         if (maybePendingVerification.isEmpty()) {
             return StatusCodes.NOT_FOUND;
         }
 
         PendingVerification pendingVerification = maybePendingVerification.get();
-        VerificationSession session = pendingVerification.verificationSession();
-        PendingStoreUtils.putForVerificationSession(
-                linkedPendingVerifications, accountId, pendingVerification, session, exchange);
+        putPendingVerification(LINKED_STORE_NAME, accountId, pendingVerification, exchange);
         return StatusCodes.OK;
     }
 
@@ -97,7 +97,8 @@ final class VerificationManagerImpl implements VerificationManager {
         }
 
         VerifiedUser user = maybeUser.get();
-        Optional<PendingVerification> maybePendingVerification = linkedPendingVerifications.tryRemove(accountId);
+        Optional<PendingVerification> maybePendingVerification =
+                tryRemovePendingVerification(LINKED_STORE_NAME, accountId);
         if (maybePendingVerification.isEmpty()) {
             return HttpOptional.empty(StatusCodes.NOT_FOUND);
         }
@@ -114,6 +115,24 @@ final class VerificationManagerImpl implements VerificationManager {
         SiteLocation location = pendingVerification.siteConfig().siteLocation();
         Verification verification = Verification.of(certificate, location);
         return HttpOptional.of(verification);
+    }
+
+    /** Puts a pending verification. */
+    private void putPendingVerification(
+            String name, String key, PendingVerification pendingVerification, HttpServerExchange exchange) {
+        PendingStore<PendingVerification> pendingStore =
+                pendingStoreFactory.getOrCreate(name, new TypeReference<>() {});
+        long expiration =
+                pendingVerification.verificationSession().verificationRequest().expiration();
+        XnioExecutor executor = exchange.getIoThread();
+        pendingStore.put(key, pendingVerification, expiration, executor);
+    }
+
+    /** Returns and removes a pending verification, or returns empty. */
+    private Optional<PendingVerification> tryRemovePendingVerification(String name, String key) {
+        PendingStore<PendingVerification> pendingStore =
+                pendingStoreFactory.getOrCreate(name, new TypeReference<>() {});
+        return pendingStore.tryRemove(key);
     }
 
     /** Creates a {@link VerificationSession} for the site. */
