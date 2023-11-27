a6d15f01f4a49912d562aee07fac1ae2fd6101cf
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-11-26 22:46:54 -0700

    add test fixtures for location types

---
+++ common-data/src/testFixtures/java/org/example/age/test/common/service/data/TestSiteLocationModule.java
@@ -0,0 +1,20 @@
+package org.example.age.test.common.service.data;
+
+import dagger.Module;
+import dagger.Provides;
+import org.example.age.common.service.data.SiteLocation;
+import org.example.age.testing.server.TestServer;
+
+/**
+ * Dagger module that publishes a binding for {@link SiteLocation}.
+ *
+ * <p>Depends on an unbound {@link TestServer}.</p>
+ */
+@Module
+public interface TestSiteLocationModule {
+
+    @Provides
+    static SiteLocation provideAvsLocation(TestServer<?> siteServer) {
+        return TestLocations.site(siteServer);
+    }
+}
