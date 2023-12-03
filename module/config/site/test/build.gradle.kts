51c2e55bca0d2b7ae627ef378baf0292efe21ebd
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-02 17:21:55 -0700

    slight change to build file

--- module/config/site/test/build.gradle.kts
+++ module/config/site/test/build.gradle.kts
@@ -9,10 +9,11 @@ dependencies {
     testFixturesAnnotationProcessor("com.google.dagger:dagger-compiler")
 
     testFixturesApi(project(":core:site:service"))
-    testFixturesApi(testFixtures(project(":module:config:common:test")))
     testFixturesApi(testFixtures(project(":testing")))
     testFixturesApi("com.google.dagger:dagger")
     testFixturesApi("javax.inject:javax.inject")
+
+    testFixturesApi(testFixtures(project(":module:config:common:test")))
 
     // test
     testAnnotationProcessor("com.google.dagger:dagger-compiler")
