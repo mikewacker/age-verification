2c8d2cc999ba98b3f89d6086b327936dc051993d
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-02 22:55:47 -0700

    split out service types into separate module (#188)

---
+++ core/common/service-types/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("org.example.age.java-conventions")
+    `java-library`
+}
+
+dependencies {
+    annotationProcessor("org.immutables:value")
+
+    api(project(":core:data"))
+    api("com.fasterxml.jackson.core:jackson-databind")
+    api("org.immutables:value-annotations")
+    api("org.jboss.xnio:xnio-api")
+}
--- core/common/service/build.gradle.kts
+++ core/common/service/build.gradle.kts
@@ -6,17 +6,15 @@ plugins {
 dependencies {
     // main
     annotationProcessor("com.google.dagger:dagger-compiler")
-    annotationProcessor("org.immutables:value")
 
     api(project(":api-types"))
     api(project(":core:common:api-types"))
+    api(project(":core:common:service-types"))
     api(project(":core:data"))
     api(project(":infra:service"))
     api("com.fasterxml.jackson.core:jackson-databind")
     api("com.google.dagger:dagger")
-    api("io.undertow:undertow-core")
     api("javax.inject:javax.inject")
-    api("org.immutables:value-annotations")
 
     // test
     testAnnotationProcessor("com.google.dagger:dagger-compiler")
