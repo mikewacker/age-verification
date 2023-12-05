afe349cd11b61269367fb3ea5032cf760f8a00cc
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-05 01:08:26 -0700

    exact deps for Dagger-less modules (#206)

--- core/common/service-types/build.gradle.kts
+++ core/common/service-types/build.gradle.kts
@@ -4,12 +4,20 @@ plugins {
 }
 
 dependencies {
+    // main
     annotationProcessor("org.immutables:value")
 
     api(project(":api-types"))
     api(project(":core:common:api-types"))
     api(project(":core:data"))
+    api("com.fasterxml.jackson.core:jackson-annotations")
     api("com.fasterxml.jackson.core:jackson-databind")
     api("org.immutables:value-annotations")
     api("org.jboss.xnio:xnio-api")
+
+    implementation("com.fasterxml.jackson.core:jackson-core")
+    implementation("com.google.guava:guava")
+
+    // test
+    testImplementation("com.fasterxml.jackson.core:jackson-core")
 }
