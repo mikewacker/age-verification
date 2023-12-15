0cb2839cb365aa315001244fe504ceed3f0e0bd3
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-15 00:03:16 -0700

    exact deps for infra, demo (#247)

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -4,10 +4,19 @@ plugins {
 }
 
 dependencies {
+    // main
+    implementation(project(":base:data:crypto"))
     implementation(project(":core:data"))
     implementation(project(":core:verification-poc"))
+    implementation("com.google.errorprone:error_prone_annotations")
     implementation("com.google.guava:guava")
     implementation("org.bouncycastle:bcpkix-jdk18on")
+    implementation("org.bouncycastle:bcprov-jdk18on")
+
+    // test
+    testImplementation(project(":base:data:crypto"))
+    testImplementation(project(":core:data"))
+    testImplementation(project(":core:verification-poc"))
 }
 
 application {
