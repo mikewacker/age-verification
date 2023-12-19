9ba305c7267ec0cfbf3281e9a15df5ff4b260cd9
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-19 16:29:00 -0700

    spotless fix

--- core/service/endpoint/avs/src/testFixtures/java/org/example/age/service/component/test/avs/TestAvsServiceModule.java
+++ core/service/endpoint/avs/src/testFixtures/java/org/example/age/service/component/test/avs/TestAvsServiceModule.java
@@ -7,8 +7,8 @@ import org.example.age.module.extractor.builtin.common.DisabledAuthMatchDataExtr
 import org.example.age.module.extractor.test.common.TestAccountIdExtractorModule;
 import org.example.age.module.key.test.common.TestKeyModule;
 import org.example.age.module.location.test.common.TestSiteLocationModule;
-import org.example.age.module.store.test.avs.TestAvsVerificationStoreModule;
 import org.example.age.module.store.inmemory.common.InMemoryPendingStoreFactoryModule;
+import org.example.age.module.store.test.avs.TestAvsVerificationStoreModule;
 import org.example.age.service.endpoint.avs.AvsServiceModule;
 
 /** Dagger module that binds dependencies for <code>@Named("api") {@link HttpHandler}</code>. */
--- core/service/endpoint/avs/src/testFixtures/java/org/example/age/service/component/test/avs/TestAvsVerificationManagerModule.java
+++ core/service/endpoint/avs/src/testFixtures/java/org/example/age/service/component/test/avs/TestAvsVerificationManagerModule.java
@@ -3,8 +3,8 @@ package org.example.age.service.component.test.avs;
 import dagger.Module;
 import org.example.age.module.config.test.avs.TestAvsConfigModule;
 import org.example.age.module.key.test.common.TestKeyModule;
-import org.example.age.module.store.test.avs.TestAvsVerificationStoreModule;
 import org.example.age.module.store.inmemory.common.InMemoryPendingStoreFactoryModule;
+import org.example.age.module.store.test.avs.TestAvsVerificationStoreModule;
 import org.example.age.service.verification.internal.avs.AvsVerificationManager;
 import org.example.age.service.verification.internal.avs.AvsVerificationManagerModule;
 
