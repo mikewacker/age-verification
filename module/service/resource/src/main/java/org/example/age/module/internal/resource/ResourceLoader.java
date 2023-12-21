fe7ce5649cd4873ca9ce6c228799a31705ac049c
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-20 21:16:20 -0700

    Dagger module to get locations from resources

---
+++ module/service/resource/src/main/java/org/example/age/module/internal/resource/ResourceLoader.java
@@ -0,0 +1,11 @@
+package org.example.age.module.internal.resource;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import java.nio.file.Path;
+
+/** Loads resource files. */
+public interface ResourceLoader {
+
+    /** Loads and deserializes a value from a JSON file. */
+    <V> V loadJson(Path path, TypeReference<V> valueTypeRef);
+}
---
+++ module/service/resource/src/test/java/org/example/age/module/internal/resource/ResourceLoaderTest.java
@@ -0,0 +1,56 @@
+package org.example.age.module.internal.resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import dagger.BindsInstance;
+import dagger.Component;
+import java.nio.file.Path;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public final class ResourceLoaderTest {
+
+    private static ResourceLoader resourceLoader;
+
+    @BeforeAll
+    public static void createResourceLoader() {
+        resourceLoader = TestComponent.createResourceLoader();
+    }
+
+    @Test
+    public void loadJson() {
+        String text = resourceLoader.loadJson(Path.of("test/test.json"), new TypeReference<>() {});
+        assertThat(text).isEqualTo("test");
+    }
+
+    @Test
+    public void error_FileNotFound() {
+        assertThatThrownBy(() -> resourceLoader.loadJson(Path.of("dne.json"), new TypeReference<String>() {}))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("resource not found: dne.json");
+    }
+
+    /** Dagger component that provides a {@link ResourceLoader}. */
+    @Component(modules = ResourceLoaderModule.class)
+    @Singleton
+    interface TestComponent {
+
+        static ResourceLoader createResourceLoader() {
+            TestComponent component =
+                    DaggerResourceLoaderTest_TestComponent.factory().create(ResourceLoaderTest.class);
+            return component.resourceLoader();
+        }
+
+        ResourceLoader resourceLoader();
+
+        @Component.Factory
+        interface Factory {
+
+            TestComponent create(@BindsInstance @Named("resources") Class<?> resourcesClass);
+        }
+    }
+}
