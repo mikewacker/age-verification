f4826a42badc49495cda41b3220ac9deffd4bd7e
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-21 18:58:03 -0700

    fail fast if resources fail to load (#293)

--- module/service/resource/src/main/java/org/example/age/module/internal/resource/JsonResourceProvider.java
+++ module/service/resource/src/main/java/org/example/age/module/internal/resource/JsonResourceProvider.java
@@ -2,31 +2,19 @@ package org.example.age.module.internal.resource;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import java.nio.file.Path;
-import java.util.function.Supplier;
 
 /** Provides a value that is read and deserialized from a JSON resource file. Is not refreshable. */
 public abstract class JsonResourceProvider<V> {
 
-    private final ResourceLoader resourceLoader;
-    private final Path path;
-    private final TypeReference<V> valueTypeRef;
-
-    private final Supplier<V> valueProvider = DoubleCheckedProvider.create(this::loadValue);
+    private final V value;
 
     /** Creates a value provider that reads and deserializes a JSON resource file. */
     protected JsonResourceProvider(ResourceLoader resourceLoader, Path path, TypeReference<V> valueTypeRef) {
-        this.resourceLoader = resourceLoader;
-        this.path = path;
-        this.valueTypeRef = valueTypeRef;
+        value = resourceLoader.loadJson(path, valueTypeRef);
     }
 
     /** Gets the value. */
     protected final V getInternal() {
-        return valueProvider.get();
-    }
-
-    /** Reads and deserializes a JSON resource file. */
-    private V loadValue() {
-        return resourceLoader.loadJson(path, valueTypeRef);
+        return value;
     }
 }
--- module/service/resource/src/main/java/org/example/age/module/internal/resource/PemResourceProvider.java
+++ module/service/resource/src/main/java/org/example/age/module/internal/resource/PemResourceProvider.java
@@ -1,34 +1,24 @@
 package org.example.age.module.internal.resource;
 
 import java.nio.file.Path;
-import java.util.function.Supplier;
 
 /** Provides a key that is read from a PEM resource file. Is not refreshable. */
 public abstract class PemResourceProvider<K> {
 
-    private final ResourceLoader resourceLoader;
-    private final Path path;
-
-    @SuppressWarnings("this-escape")
-    private final Supplier<K> keyProvider = DoubleCheckedProvider.create(this::loadKey);
+    private final K key;
 
     /** Gets the key. */
     public final K get() {
-        return keyProvider.get();
+        return key;
     }
 
     /** Creates a key provider that reads a PEM resource file. */
+    @SuppressWarnings("this-escape")
     protected PemResourceProvider(ResourceLoader resourceLoader, Path path) {
-        this.resourceLoader = resourceLoader;
-        this.path = path;
+        Object pemObject = resourceLoader.loadPem(path);
+        key = createKey(pemObject);
     }
 
     /** Creates a key from a PEM object. */
     protected abstract K createKey(Object pemObject);
-
-    /** Reads a PEM resource file and creates a key from it. */
-    private K loadKey() {
-        Object pemObject = resourceLoader.loadPem(path);
-        return createKey(pemObject);
-    }
 }
