be8633d749aa50427d48728c083774dee80ad3e5
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2023-12-22 23:25:16 -0700

    redesigned API client

--- infra/client/build.gradle.kts
+++ infra/client/build.gradle.kts
@@ -5,11 +5,17 @@ plugins {
 
 dependencies {
     // main
+    api(project(":base:api:base"))
+    api("com.fasterxml.jackson.core:jackson-core")
     api("com.squareup.okhttp3:okhttp")
 
     implementation(project(":base:data:json"))
 
     // test
+    testImplementation(project(":base:api:base"))
+    testImplementation(testFixtures(project(":base:api:base")))
+    testImplementation("com.fasterxml.jackson.core:jackson-core")
     testImplementation("com.squareup.okhttp3:mockwebserver")
+    testImplementation("com.squareup.okhttp3:okhttp")
     testImplementation("com.squareup.okio:okio-jvm")
 }
