a03b214dfacd9658c77d7da97e3695af40cfeef3
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2024-12-29 17:18:11 -0700

    adapt async call to completion stage

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,5 +5,10 @@ plugins {
 
 dependencies {
     api(project(":api"))
+    implementation(libs.jakartaInject.api)
     implementation(libs.jaxRs.api)
+
+    testImplementation(libs.retrofit.mock)
+
+    testRuntimeOnly(libs.dropwizard.core) // provides RuntimeDelegate for JAX-RS Response
 }
