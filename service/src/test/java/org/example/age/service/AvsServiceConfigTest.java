7b6967a7b0eec0aa1110f9a26a8b4121a16bf411
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2024-12-31 20:07:23 -0700

    fixes

--- service/src/test/java/org/example/age/service/AvsServiceConfigTest.java
+++ service/src/test/java/org/example/age/service/AvsServiceConfigTest.java
@@ -2,22 +2,20 @@ package org.example.age.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.time.Duration;
-import org.example.age.service.testing.TestObjectMapper;
+import org.example.age.testing.TestObjectMapper;
 import org.junit.jupiter.api.Test;
 
 public final class AvsServiceConfigTest {
 
     @Test
     public void serializeThenDeserialize() throws IOException {
-        ObjectMapper mapper = TestObjectMapper.get();
         AvsServiceConfig config = AvsServiceConfig.builder()
                 .verificationRequestExpiresIn(Duration.ofMinutes(5))
                 .build();
-        String json = mapper.writeValueAsString(config);
-        AvsServiceConfig rtConfig = mapper.readValue(json, AvsServiceConfig.class);
+        String json = TestObjectMapper.get().writeValueAsString(config);
+        AvsServiceConfig rtConfig = TestObjectMapper.get().readValue(json, AvsServiceConfig.class);
         assertThat(rtConfig).isEqualTo(config);
     }
 }
