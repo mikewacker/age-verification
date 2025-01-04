7ef7ae31e85138e9a40bc0b4867bda5ef2155107
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-01-03 23:23:51 -0700

    avs client module (#337)

---
+++ module/client/src/main/java/org/example/age/module/client/SiteClientRepositoryImpl.java
@@ -0,0 +1,36 @@
+package org.example.age.module.client;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.NotAuthorizedException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.example.age.api.client.SiteApi;
+import org.example.age.service.api.client.SiteClientRepository;
+
+/** Implementation of {@link SiteClientRepository}. */
+@Singleton
+final class SiteClientRepositoryImpl implements SiteClientRepository {
+
+    private final Map<String, SiteApi> clients;
+
+    @Inject
+    public SiteClientRepositoryImpl(ServiceClientFactory clientFactory, AvsClientsConfig clientsConfig) {
+        clients = createClients(clientFactory, clientsConfig);
+    }
+
+    @Override
+    public SiteApi get(String siteId) {
+        Optional<SiteApi> maybeClient = Optional.ofNullable(clients.get(siteId));
+        return maybeClient.orElseThrow(() -> new NotAuthorizedException("unregistered site"));
+    }
+
+    /** Creates the clients. */
+    private static Map<String, SiteApi> createClients(
+            ServiceClientFactory clientFactory, AvsClientsConfig clientsConfig) {
+        return clientsConfig.siteUrls().entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey, entry -> clientFactory.create(entry.getValue(), SiteApi.class)));
+    }
+}
