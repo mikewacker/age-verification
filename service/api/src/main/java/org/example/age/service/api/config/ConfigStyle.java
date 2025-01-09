7b4c9d073560bff73254a2a04ba61b91d77ba4ce
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-01-09 15:41:52 -0700

    finetune config style

--- service/api/src/main/java/org/example/age/service/api/config/ConfigStyle.java
+++ service/api/src/main/java/org/example/age/service/api/config/ConfigStyle.java
@@ -1,7 +1,14 @@
 package org.example.age.service.api.config;
 
+import org.immutables.value.Generated;
 import org.immutables.value.Value;
 
 /** Style annotation for configuration interfaces. */
-@Value.Style(visibility = Value.Style.ImplementationVisibility.PACKAGE, overshadowImplementation = true)
+@Value.Style(
+        visibility = Value.Style.ImplementationVisibility.PACKAGE,
+        overshadowImplementation = true,
+        jdk9Collections = true,
+        allowedClasspathAnnotations = {Generated.class, javax.annotation.processing.Generated.class},
+        defaults = @Value.Immutable(copy = false),
+        from = "")
 public @interface ConfigStyle {}
