eb12156e213ae0163cfb7f0cbe3d0ab13457d7a4
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-01-10 13:35:31 -0700

    delete old code (#356)

--- core/api/extractors/src/main/java/org/example/age/api/extractor/AuthMatchDataExtractor.java
+++
@@ -1,9 +0,0 @@
-package org.example.age.api.extractor;
-
-import io.github.mikewacker.drift.endpoint.ArgExtractor;
-import io.undertow.server.HttpServerExchange;
-import org.example.age.api.def.AuthMatchData;
-
-/** Extracts {@link AuthMatchData} from an {@link HttpServerExchange}, or returns an error status code. */
-@FunctionalInterface
-public interface AuthMatchDataExtractor extends ArgExtractor<HttpServerExchange, AuthMatchData> {}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,6 @@
 rootProject.name = "age-verification"
 
 include(
-        // new
         "api",
         "api:crypto",
         "service",
@@ -12,27 +11,5 @@ include(
         "module:store-demo",
         "module:crypto-demo",
         "app",
-
         "testing",
-
-        // legacy
-        "crypto:data",
-
-        "core:data",
-        "core:api:types",
-        "core:api:extractors",
-        "core:api:endpoint",
-        "core:service:types",
-        "core:service:crypto",
-        "core:service:endpoint",
-        "core:server",
-
-        "module:extractor:builtin",
-        "module:extractor:demo",
-        "module:extractor:test",
-        "module:store:inmemory",
-        "module:service:resource",
-        "module:service:test",
-
-        "demo",
 )
