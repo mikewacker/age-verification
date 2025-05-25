f7424acfe3f3ee59dafffb5aba9f939c09bf62b4
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-05-25 15:47:26 -0600

    new test utils for async web stages (#404)

---
+++ testing/src/main/java/org/example/age/testing/WebStageTesting.java
@@ -0,0 +1,104 @@
+package org.example.age.testing;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import jakarta.ws.rs.WebApplicationException;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.mock.Calls;
+
+/** Test utilities for an asynchronous stage that is returned by a JAX-RS API. */
+public final class WebStageTesting {
+
+    private static final Duration DEFAULT_TIMEOUT = Duration.ofSeconds(5);
+
+    /** Waits for the asynchronous stage to complete and returns its value, asserting that it completed successfully. */
+    public static <V> V await(CompletionStage<V> stage) {
+        return await(stage, DEFAULT_TIMEOUT);
+    }
+
+    /** Waits for the asynchronous stage to complete and returns its value, asserting that it completed successfully. */
+    public static <V> V await(CompletionStage<V> stage, Duration timeout) {
+        try {
+            return get(stage, timeout);
+        } catch (ExecutionException execE) {
+            Throwable cause = execE.getCause();
+            int errorCode = getErrorCode(cause);
+            String message = String.format("%d error", errorCode);
+            fail(message, cause);
+        } catch (TimeoutException | InterruptedException e) {
+            fail(e);
+        }
+        return null;
+    }
+
+    /** Waits for the asynchronous stage to complete, asserting that it completed with an HTTP error code. */
+    public static void awaitErrorCode(CompletionStage<?> stage, int expectedErrorCode) {
+        awaitErrorCode(stage, expectedErrorCode, DEFAULT_TIMEOUT);
+    }
+
+    /** Waits for the asynchronous stage to complete, asserting that it completed with an HTTP error code. */
+    public static void awaitErrorCode(CompletionStage<?> stage, int expectedErrorCode, Duration timeout) {
+        try {
+            get(stage, timeout);
+            String message = String.format("stage succeeded, expected a %d error", expectedErrorCode);
+            fail(message);
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            int errorCode = getErrorCode(cause);
+            if (errorCode == expectedErrorCode) {
+                return;
+            }
+
+            String message =
+                    String.format("stage failed with a %d error, expected a %d error", errorCode, expectedErrorCode);
+            fail(message, cause);
+        } catch (TimeoutException | InterruptedException e) {
+            fail(e);
+        }
+    }
+
+    /** Converts an asynchronous stage to an asynchronous Retrofit call. */
+    public static <V> Call<V> toCall(CompletionStage<V> stage) {
+        return toCall(stage, DEFAULT_TIMEOUT);
+    }
+
+    /** Converts an asynchronous stage to an asynchronous Retrofit call. */
+    public static <V> Call<V> toCall(CompletionStage<V> stage, Duration timeout) {
+        return Calls.defer(() -> completeAsCall(stage, timeout));
+    }
+
+    /** Completes the asynchronous stage, converting the result to a Retrofit call. */
+    private static <V> Call<V> completeAsCall(CompletionStage<V> stage, Duration timeout) {
+        try {
+            V value = get(stage, timeout);
+            return Calls.response(value);
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            int errorCode = getErrorCode(cause);
+            Response<V> response = Response.error(errorCode, ResponseBody.create("", null));
+            return Calls.response(response);
+        } catch (TimeoutException | InterruptedException e) {
+            return Calls.failure(e);
+        }
+    }
+
+    /** Gets the result of an asynchronous stage, subject to a timeout. */
+    private static <V> V get(CompletionStage<V> stage, Duration timeout)
+            throws ExecutionException, TimeoutException, InterruptedException {
+        return stage.toCompletableFuture().get(timeout.toMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    /** Gets the HTTP error code. */
+    private static int getErrorCode(Throwable t) {
+        return (t instanceof WebApplicationException e) ? e.getResponse().getStatus() : 500;
+    }
+
+    private WebStageTesting() {} // static class
+}
---
+++ testing/src/test/java/org/example/age/testing/WebStageTestingTest.java
@@ -0,0 +1,133 @@
+package org.example.age.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.example.age.testing.WebStageTesting.await;
+import static org.example.age.testing.WebStageTesting.awaitErrorCode;
+
+import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.NotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.Test;
+import retrofit2.Call;
+import retrofit2.Response;
+
+public final class WebStageTestingTest {
+
+    @Test
+    public void await_Success() {
+        CompletionStage<String> stage = CompletableFuture.completedStage("value");
+        String value = await(stage);
+        assertThat(value).isEqualTo("value");
+    }
+
+    @Test
+    public void await_ErrorCode() {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new NotFoundException());
+        assertThatThrownBy(() -> await(stage))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(NotFoundException.class)
+                .hasMessage("404 error");
+    }
+
+    @Test
+    public void await_Exception() {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new IllegalStateException());
+        assertThatThrownBy(() -> await(stage))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(IllegalStateException.class)
+                .hasMessage("500 error");
+    }
+
+    @Test
+    public void await_Timeout() {
+        CompletionStage<?> stage = CompletableFuture.supplyAsync(WebStageTestingTest::sleep);
+        assertThatThrownBy(() -> await(stage, Duration.ofMillis(1)))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(TimeoutException.class)
+                .hasMessage("");
+    }
+
+    @Test
+    public void awaitErrorCode_Success() {
+        CompletionStage<?> stage = CompletableFuture.completedStage("value");
+        assertThatThrownBy(() -> awaitErrorCode(stage, 404))
+                .isInstanceOf(AssertionError.class)
+                .hasMessage("stage succeeded, expected a 404 error");
+    }
+
+    @Test
+    public void awaitErrorCode_ExpectedErrorCode() {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new NotFoundException());
+        awaitErrorCode(stage, 404);
+    }
+
+    @Test
+    public void awaitErrorCode_UnexpectedErrorCode() {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new ForbiddenException());
+        assertThatThrownBy(() -> awaitErrorCode(stage, 404))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(ForbiddenException.class)
+                .hasMessage("stage failed with a 403 error, expected a 404 error");
+    }
+
+    @Test
+    public void awaitErrorCode_Exception() {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new IllegalStateException());
+        assertThatThrownBy(() -> awaitErrorCode(stage, 404))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(IllegalStateException.class)
+                .hasMessage("stage failed with a 500 error, expected a 404 error");
+    }
+
+    @Test
+    public void awaitErrorCode_Timeout() {
+        CompletionStage<?> stage = CompletableFuture.supplyAsync(WebStageTestingTest::sleep);
+        assertThatThrownBy(() -> awaitErrorCode(stage, 404, Duration.ofMillis(1)))
+                .isInstanceOf(AssertionError.class)
+                .hasCauseInstanceOf(TimeoutException.class)
+                .hasMessage("");
+    }
+
+    @Test
+    public void toCall_Success() throws IOException {
+        CompletionStage<String> stage = CompletableFuture.completedStage("value");
+        Response<String> response = WebStageTesting.toCall(stage).execute();
+        assertThat(response.isSuccessful()).isTrue();
+        assertThat(response.body()).isEqualTo("value");
+    }
+
+    @Test
+    public void toCall_ErrorCode() throws IOException {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new NotFoundException());
+        Response<?> response = WebStageTesting.toCall(stage).execute();
+        assertThat(response.code()).isEqualTo(404);
+    }
+
+    @Test
+    public void toCall_Exception() throws IOException {
+        CompletionStage<?> stage = CompletableFuture.failedStage(new IllegalStateException());
+        Response<?> response = WebStageTesting.toCall(stage).execute();
+        assertThat(response.code()).isEqualTo(500);
+    }
+
+    @Test
+    public void toCall_Timeout() {
+        CompletionStage<?> stage = CompletableFuture.supplyAsync(WebStageTestingTest::sleep);
+        Call<?> call = WebStageTesting.toCall(stage, Duration.ofMillis(1));
+        assertThatThrownBy(call::execute).isInstanceOf(TimeoutException.class);
+    }
+
+    private static Void sleep() {
+        try {
+            Thread.sleep(3);
+            return null;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
