77bea89282920efe2a9ce8311242b13cfb21e58e
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-05-26 16:33:43 -0600

    add tests

---
+++ testing/src/test/java/org/example/age/testing/TestObjectMapperTest.java
@@ -0,0 +1,16 @@
+package org.example.age.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+
+public final class TestObjectMapperTest {
+
+    @Test
+    public void get() {
+        ObjectMapper mapper = TestObjectMapper.get();
+        assertThat(mapper).isNotNull();
+        assertThat(mapper).isSameAs(TestObjectMapper.get());
+    }
+}
