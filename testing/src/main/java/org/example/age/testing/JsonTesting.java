5bd82d73828436d1fa158a57ac40c4bf3c3128b5
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-05-26 16:20:02 -0600

    refactor json testing utils

--- testing/src/main/java/org/example/age/testing/JsonTesting.java
+++ testing/src/main/java/org/example/age/testing/JsonTesting.java
@@ -9,15 +9,15 @@ public final class JsonTesting {
 
     /** Serializes then deserializes an object, asserting that the deserialized object equals the original object. */
     public static <V> void serializeThenDeserialize(V value, Class<V> valueType) throws IOException {
-        String json = TestObjectMapper.get().writeValueAsString(value);
-        V rtValue = TestObjectMapper.get().readValue(json, valueType);
+        String json = serializeInternal(value);
+        V rtValue = deserializeInternal(json, valueType);
         assertThat(rtValue).isEqualTo(value);
     }
 
     /** Serializes an object to JSON. */
     public static String serialize(Object value) {
         try {
-            return TestObjectMapper.get().writeValueAsString(value);
+            return serializeInternal(value);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -26,10 +26,20 @@ public final class JsonTesting {
     /** Deserializes an object from JSON. */
     public static <V> V deserialize(String json, Class<V> valueType) {
         try {
-            return TestObjectMapper.get().readValue(json, valueType);
+            return deserializeInternal(json, valueType);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    /** Serializes an object to JSON. */
+    private static String serializeInternal(Object value) throws IOException {
+        return TestObjectMapper.get().writeValueAsString(value);
+    }
+
+    /** Deserializes an object from JSON. */
+    private static <V> V deserializeInternal(String json, Class<V> valueType) throws IOException {
+        return TestObjectMapper.get().readValue(json, valueType);
     }
 
     private JsonTesting() {} // static class
