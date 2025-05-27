3a30f04d51d7f0ab5aa43b0c10ffbaaa01dff6ba
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-05-26 20:15:09 -0600

    put demo in separate module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,23 @@
 rootProject.name = "age-verification"
 
 include(
+        // interfaces
         "api",
-        "service",
         "service:module",
+
+        // implementations
+        "service",
         "module:request-demo",
         "module:client",
         "module:store-redis",
         "module:store-dynamodb",
         "module:crypto-demo",
+
+        // app
         "app",
+        "demo",
+
+        // test
         "testing",
         "test-containers",
 )
