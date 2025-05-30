ec0cb7fa6672dd319e451b6a1155ac60548bbab1
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-05-29 19:22:48 -0600

    add base test fixture for a test container (#424)

---
+++ module/common/src/test/java/org/example/age/module/common/testing/BaseTestContainerTest.java
@@ -0,0 +1,82 @@
+package org.example.age.module.common.testing;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public final class BaseTestContainerTest {
+
+    @Test
+    public void testLifecycle() throws Exception {
+        FakeTestContainer container = new FakeTestContainer();
+        container.beforeAll(null);
+        FakeClient client = container.getClient();
+        assertThat(client.isClean()).isTrue();
+
+        client.doOps();
+        container.afterAll(null);
+        assertThat(client.isClean()).isTrue();
+        assertThat(client.isClosed()).isTrue();
+    }
+
+    @Test
+    public void error_NotInitialized() {
+        FakeTestContainer container = new FakeTestContainer();
+        assertThatThrownBy(container::getClient).isInstanceOf(IllegalStateException.class);
+    }
+
+    /** Fake test container. */
+    private static final class FakeTestContainer extends BaseTestContainer<FakeClient> {
+
+        @Override
+        protected FakeClient createClient() {
+            return new FakeClient();
+        }
+
+        @Override
+        protected void clean(FakeClient client) {
+            client.clean();
+        }
+
+        @Override
+        protected void closeClient(FakeClient client) {
+            client.close();
+        }
+    }
+
+    /** Fake client that simulates having a clean or dirty container. */
+    private static final class FakeClient {
+
+        private boolean isClean = false;
+        private boolean isClosed = false;
+
+        public boolean isClean() {
+            return isClean;
+        }
+
+        public boolean isClosed() {
+            return isClosed;
+        }
+
+        public void doOps() {
+            checkIsNotClosed();
+            isClean = false;
+        }
+
+        public void clean() {
+            checkIsNotClosed();
+            isClean = true;
+        }
+
+        public void close() {
+            isClosed = true;
+        }
+
+        private void checkIsNotClosed() {
+            if (isClosed) {
+                throw new IllegalStateException("closed");
+            }
+        }
+    }
+}
