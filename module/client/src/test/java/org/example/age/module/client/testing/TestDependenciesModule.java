5f88f5348ba1017f73e69200a994e35221458ccf
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2025-10-04 17:56:32 -0600

    fix

--- module/client/src/test/java/org/example/age/module/client/testing/TestDependenciesModule.java
+++ module/client/src/test/java/org/example/age/module/client/testing/TestDependenciesModule.java
@@ -4,24 +4,17 @@ import dagger.Module;
 import dagger.Provides;
 import org.example.age.common.env.LiteEnv;
 import org.example.age.module.client.AvsClientsConfig;
-import org.example.age.module.client.SiteClientsConfig;
 import org.example.age.testing.env.TestEnvModule;
 
 /**
  * Dagger module that binds...
  * <ul>
- *     <li>{@link SiteClientsConfig}
  *     <li>{@link AvsClientsConfig}
  *     <li>{@link LiteEnv}
  * </ul>
  */
 @Module(includes = TestEnvModule.class)
 public interface TestDependenciesModule {
-
-    @Provides
-    static SiteClientsConfig provideSiteClientsConfig() {
-        return TestConfig.siteClients();
-    }
 
     @Provides
     static AvsClientsConfig provideAvsClientsConfig() {
